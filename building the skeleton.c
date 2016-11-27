f08f2f580d2b989477c74bfc9357b8b5f986eff9
Calson Noah <chiacalson@gmail.com>
2016-11-27 19:24:15 +0100

    adds a function to enable play with numbers 1-9

--- building the skeleton.c
+++ building the skeleton.c
@@ -42,6 +42,56 @@ void getPlayerName(void)//used to get the name of the players
         printf("\t\t%s (player X) Vs %s (player O)\n\n", player1,player2);
         }
 }
+
+void convert2DTo1D(void)//makes it possible to use intergers 1 to 9 instead of using rows and columns
+{
+             switch(Cordinate)
+            {
+                      case 1:
+                      Row=0;
+                      Column=0;
+                      break;
+                      case 2:
+                      Row=0;
+                      Column=1;
+                      break;
+                      case 3:
+                      Row=0;
+                      Column=2;
+                      break;
+                      case 4:
+                      Row=1;
+                      Column=0;
+                      break;
+                      case 5:
+                      Row=1;
+                      Column=1;
+                      break;
+                      case 6:
+                      Row=1;
+                      Column=2;
+                      break;
+                      case 7:
+                      Row=2;
+                      Column=0;
+                      break;
+                      case 8:
+                      Row=2;
+                      Column=1;
+                      break;
+                      case 9:
+                      Row=2;
+                      Column=2;
+                      break;
+                      default:
+                      Row=3;
+                      Column=3;
+                      break;
+
+                    }
+}
+
+
 int main(void)
 {
     return 0;
